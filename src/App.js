77bd33985c64a29bbafea70653279cfabfa39904
Swastika <swastikat.18@gmail.com>
2019-05-31 23:53:37 +0530

    Some Minor Changes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React, {Component} from 'react';
 import CardList from './CardList';
-import {robots} from './robots';
 import SearchBox from './SearchBox';
+import Scroll from './Scroll';
 import './App.css'
 
 class App extends Component{
 	constructor(){
 		super()
 		this.state={
-			robots: robots,
+			robots: [],
 			searchfield:''
 		}
 	}
@@ -17,14 +17,24 @@ class App extends Component{
 		this.setState({searchfield: event.target.value});
 		
 	}
-
+	componentDidMount(){
+		fetch('https://jsonplaceholder.typicode.com/users')
+		.then(
+			response=>{return response.json();}
+			)
+		.then(
+			users=>{this.setState({robots: users});}
+			);
+	}
 	render(){
 		const filteredRobots= this.state.robots.filter(robot=>{return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase())});
 		return(
 		<div className='tc'>
 			<h1 id="robo">ROBOFRIENDS</h1>
 			<SearchBox searchChange={this.onSearchChange}/>
-			<CardList robots={filteredRobots} />
+			<Scroll>
+				<CardList robots={filteredRobots} />
+			</Scroll>
 		</div>
 		);
 	};
